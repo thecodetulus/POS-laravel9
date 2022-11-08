8f15f3d44be54955c23186d91d50525e86067bba
thecodetulus <tuluscodeportofolio@gmail.com>
2022-11-08 23:34:05 +0700

    custome template

---
+++ resources/views/components/sidebaradmin.blade.php
@@ -0,0 +1,21 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.html">Stisla</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.html">St</a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="menu-header">Dashboard</li>
+        <li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Dashboard</span></a></li>
+        
+        <li class="menu-header">Data Menu</li>
+        
+        <li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Data Category</span></a></li>
+        <li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Data Product</span></a></li>
+        <li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Order</span></a></li>
+        <li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Transaction</span></a></li>
+      </ul>
+
+      
+  </aside>
